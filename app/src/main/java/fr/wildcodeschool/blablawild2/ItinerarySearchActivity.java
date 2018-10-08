d5c0e996e9c3e283a4030767a0f0df744ba36dae
Jérémy Fruit <jeremy.fruit@gmail.com>
2018-10-08 16:40:32 +0200

    QUEST_FIREBASE_BASIC

--- app/src/main/java/fr/wildcodeschool/blablawild2/ItinerarySearchActivity.java
+++ app/src/main/java/fr/wildcodeschool/blablawild2/ItinerarySearchActivity.java
@@ -8,6 +8,12 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
 public class ItinerarySearchActivity extends AppCompatActivity {
 
     public static final String EXTRA_TRIP = "EXTRA_TRIP";
@@ -34,13 +40,37 @@ public class ItinerarySearchActivity extends AppCompatActivity {
                 if (departure.isEmpty() || destination.isEmpty() || date.isEmpty()) {
                     Toast.makeText(ItinerarySearchActivity.this, R.string.fill_all_fields, Toast.LENGTH_SHORT).show();
                 } else {
-                    Intent intent = new Intent(ItinerarySearchActivity.this, ItineraryListActivity.class);
-                    TripModel tripModel = new TripModel(departure, destination, date);
+                    final Intent intent = new Intent(ItinerarySearchActivity.this, ItineraryListActivity.class);
+                    final TripModel tripModel = new TripModel(departure, destination, date);
+                    // TODO surveiller que le tripodel a bien été ajouter dans FB
+                    FirebaseDatabase database = FirebaseDatabase.getInstance();
+                    DatabaseReference tripRef = database.getReference("trips");
+                    String key = tripRef.push().getKey();
+                    // lecture en cas de modification de la valeur
+                    tripRef.child(key).addListenerForSingleValueEvent(new ValueEventListener() {
+                        @Override
+                        public void onDataChange(DataSnapshot dataSnapshot) {
+                            intent.putExtra(EXTRA_TRIP, tripModel);
+                            startActivity(intent);
+                        }
 
-                    intent.putExtra(EXTRA_TRIP, tripModel);
-                    startActivity(intent);
+                        @Override
+                        public void onCancelled(DatabaseError error) {
+                            // en cas d'erreur de récupération de la donnée
+                            Toast.makeText(ItinerarySearchActivity.this, "Failed to read value.", Toast.LENGTH_LONG).show();
+                        }
+                    });
+                    //TODO ajouter le tripmodel dans FB
+                    // sauvegarde la valeur
+                    tripRef.child(key).setValue(tripModel);
+
+
+
                 }
             }
         });
+
+
+
     }
 }
